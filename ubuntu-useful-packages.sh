887cbe915b94e030d5bd2446d7c70f33f21dc98a
Catalin Iacob <iacobcatalin@gmail.com>
2011-10-16 19:28:06 +0200

    Initial commit.
    Some scripts for installing Ubuntu (Oneiric) packages and starting a virtualenv
    with development packages from their VCS instead of stable versions from PyPI.

---
+++ ubuntu-useful-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo aptitude install \
+ack-grep \
+emacs emacs-goodies-el \
+git-core gitk git-gui \
+mercurial \
+openssh-server \
+postgresql postgresql-client libpq-dev \
+subversion \
+vim
