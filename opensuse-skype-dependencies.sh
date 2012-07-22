d66ad0394d42a52ebae2dac2fab051e694371ae0
Catalin Iacob <iacobcatalin@gmail.com>
2012-07-22 13:58:11 +0200

    Add scripts to install packages
    These are packages I surely want on a new installation.

---
+++ opensuse-skype-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo zypper install \
+fontconfig-32bit \
+libasound2-32bit \
+libgobject-2_0-0-32bit \
+libgthread-2_0-0-32bit \
+libpulse0-32bit \
+libSM6-32bit \
+libtiff5-32bit \
+libXrender1-32bit \
+libXss1-32bit \
+libXv1-32bit
---
+++ opensuse-useful-packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo zypper install \
+emacs \
+htop \
+mercurial \
+gcc \
+git git-daemon git-gui gitk \
+make \
+ncurses-devel \
+osc
