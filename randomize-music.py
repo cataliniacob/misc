cb1142d5ac8d144e5ab0fc95ceed156c855b6bd2
Catalin Iacob <iacobcatalin@gmail.com>
2014-01-05 12:36:15 +0100

    Generalize randomize script to work recursively and on more than just music

--- randomize-music.py
+++ randomize-music.py
@@ -8,10 +8,12 @@ import uuid
 if __name__ == '__main__':
     dir_name = sys.argv[1]
 
-    for file_name in os.listdir(dir_name):
-        rand_name = uuid.uuid4().hex
-        src = os.path.join(dir_name, file_name)
-        subprocess.check_call(['eyeD3', '--artist', rand_name, '--album', rand_name, src])
+    for root, dirs, files in os.walk(dir_name):
+        for file_name in files:
+            rand_name = uuid.uuid4().hex
+            src = os.path.join(root, file_name)
+            if src.endswith('.mp3'):
+                subprocess.check_call(['eyeD3', '--artist', rand_name, '--album', rand_name, src])
 
-        os.rename(src, os.path.join(dir_name, '{} {}'.format(rand_name, file_name)))
+            os.rename(src, os.path.join(root, '{} {}'.format(rand_name, file_name)))
 
